ec15fd52945bf82b0b3c5237956c1f7f2417af14
fender190 <jeancarlosky1987@gmail.com>
2020-12-22 02:55:47 -0500

    Fixed layers, added outdoors layer, fixed legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,7 +4,7 @@ var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_wee
 
 // center coordinates, Los Angeles
 // Los Angeles, CA is in an earthquake prone area
-var centercoor = [34.0522, -118.2437];
+var centercoor = [34.052235, -118.243683];
 
 // Features
 d3.json(geoData, data => {
@@ -16,7 +16,7 @@ function createFeatures(earthquakeData){
         layer.bindPopup(
             "<h3 align='center'>" +
             feature.properties.place +
-            "</h3><hr><p><b>Occurrence:</b> " +
+            "</h3><hr><p><b>Date and Time:</b> " +
             new Date(feature.properties.time) +
             "</p>" +
             "</h3><p><b>Magnitude:</b> " +
@@ -46,104 +46,108 @@ function createFeatures(earthquakeData){
 // Map layers
 function createMap(earthquakes) {
     var lightMap = L.tileLayer(
-        
-    )
-
-}
-
-///////##############
-
-var sat = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.satellite",
-    accessToken: API_KEY
-});
-
-
-function choosecolor(mag) {
-    var color = "";
-
-    if (mag >= 8.0) {
-        color = "darkred";
-    }
-    else if (mag >= 7.0) {
-        color = "red";
-    }
-    else if (mag >= 6.0) {
-        color = "orange";
-    }
-    else if (mag >= 5.0) {
-        color = "yellow";
-    }
-    else if (mag >= 4.0) {
-        color = "lightgreen";
-    }
-    else {
-        color = "green";
-    }
-    return color
-}
-
-function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><h3>" + feature.properties.mag + " magnitude</h3><hr><p>"
-        + new Date(feature.properties.time) + "</p>")
-}
-
-var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
-
-// Perform a GET request to the query URL
-d3.json(link, function (data) {
-    //check data
-    console.log(data.features);
-
-    //    add geojson layer
-    L.geoJson(data.features, { onEachFeature: onEachFeature,
-        pointToLayer: function (feature, latlng) {
-            return L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-                color: choosecolor(feature.properties.mag),
-                fillColor: choosecolor(feature.properties.mag),
-                fillOpacity: 0.75,
-                radius: (feature.properties.mag) * 20000
-            });
+        "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}",
+        {
+            attribution:
+                'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 12,
+            id: "mapbox.light",
+            accessToken: API_KEY
         }
-    }).addTo(map);
-});
+    );
 
-var map = L.map("map", {
-    center: [14.7194, -92.4256],
-    zoom: 5,
-    layers: [out]
-  });
+    var darkMap = L.tileLayer(
+        "https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}",
+        {
+            attribution:
+                'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 12,
+            id: "mapbox.dark",
+            accessToken: API_KEY
+        }
+    );
 
-var baseMaps = {
-    Satellite: sat,
-    Light: light,
-    Outdoors: out,
-};
+    var satelliteMap = L.tileLayer(
+        "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}",
+        {
+            attribution:
+                'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 12,
+            id: "mapbox.satellite",
+            accessToken: API_KEY
+        }
+    );
 
-L.control.layers(baseMaps, null, {collapsed: false}).addTo(map);
+    var outdoorsMap = L.tileLayer(
+        "https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}",
+        {
+            attribution:
+                'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 12,
+            id: "mapbox.outdoors",
+            accessToken: API_KEY
+        }
+    );
 
-var legend = L.control({position: 'bottomright'});
+    //Base Maps
 
-legend.onAdd = function (map) {
+    var baseMaps = {
+        "Light Map": lightMap,
+        "Dark Map": darkMap,
+        "Satellite Map": satelliteMap,
+        "Outdoors Map": outdoorsMap
+    };
 
-    var div = L.DomUtil.create('div', 'info legend');
-    var grades = [2,4,5,6,7,8];
-    var labels = ["Minor", "Light", "Moderate", "Strong", "Major", "Great"];
+    //Overlay Maps
+    var overlayMaps = {
+        Earthquakes: earthquakes
+    };
 
-    div.innerHTML = "<h4>Richter Scale</h4>";
-  
+    //Creating and Centering Map
+    var map = L.map("map", {
+        center: centercoor,
+        zoom: 4,
+        layers: [lightMap, earthquakes]
+    });
 
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + choosecolor(grades[i]) + '"></i>' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
+    //Controls for layers
+    L.control.layers(baseMaps, overlayMaps, { collapsed: false}).addTo(map);
 
-    return div;
-};
+    // Map Legend
+    var legend = L.control({ position:"bottomright" });
 
-legend.addTo(map);
+    legend.onAdd = map => {
+        var div = L.DomUtil.create("div", "legend"),
+            grades = [0, 1, 2, 3, 4, 5],
+            labels = [];
+        div.innerHTML += "<b>Scale</b><br><hr>";
+        grades.forEach(i => {
+            div.innerHTML +=
+                '<i style="background:' +
+                getColor(grades[i] + 1) +
+                '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+                grades[i] +
+                (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        });
+        return div;
+    };
+
+    legend.addTo(map);
+}
+
+// Colors definition by Earthquake magnitudes
+function getColor(color) {
+    return color <= 1
+        ? "#00FF00" // green
+        : color <= 2
+        ? "#C2FF00" // light green
+        : color <= 3
+        ? "#FFFF00" // yellow
+        : color <= 4
+        ? "#FFC100" // light orange
+        : color <= 5
+        ? "#FF7B00" // darker orange
+        : color >= 5
+        ? "#FF0000" // red
+        : "#FF0000";
+}
